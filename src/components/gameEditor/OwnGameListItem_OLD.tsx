af1b963beb0943e089cf92f44bdc70dec9744357
Ian Hornik <yiliansource@gmail.com>
2023-04-23 20:13:18 +0200

    wip

---
+++ src/components/gameEditor/OwnGameListItem_OLD.tsx
@@ -0,0 +1,70 @@
+import copy from "clipboard-copy";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import {
+    MdDelete as DeleteIcon,
+    MdEdit as EditIcon,
+    MdLink as LinkIcon,
+    MdList as ListIcon,
+    MdPlayArrow as PlayIcon,
+} from "react-icons/md";
+
+import { IconButton } from "@components/util/Button";
+import { Game, WithRefPart } from "@lib/firebase/firestoreTypes";
+
+export interface OwnGameListItemProps {
+    game: WithRefPart<Game>;
+
+    onDeleteClick?(): void;
+}
+
+export function OwnGameListItem({ game, onDeleteClick }: OwnGameListItemProps) {
+    const router = useRouter();
+
+    let categories = game.categories.join(", ");
+    if (categories.length > 65) {
+        categories = categories.substring(0, 64) + "...";
+    }
+
+    const actions = [
+        { icon: PlayIcon, onClick: () => router.push(`/b/${game.id}`) },
+        {
+            icon: LinkIcon,
+            onClick: () => {
+                copy(window.origin + "/b/" + game.id)
+                    .then(() => toast.success("Bingo link copied to clipboard!"))
+                    .catch(() => toast.error("Copying game link failed."));
+            },
+        },
+        { icon: EditIcon, onClick: () => router.push(`/b/${game.id}/edit`) },
+        { icon: DeleteIcon, onClick: () => onDeleteClick?.() },
+    ];
+
+    return (
+        <div className="flex flex-row px-6 py-3 bg-white border-b items-center">
+            <div>
+                <div>
+                    <span className="font-bold text-green-800">{game.name}</span>
+                    <span className="ml-2 text-sm text-gray-400">({game.id})</span>
+                </div>
+                <div>
+                    <span>
+                        <span className="inline-block mr-1 align-middle">
+                            <ListIcon />
+                        </span>
+                        <span className="text-sm">
+                            Categories: <span className="text-gray-600">{categories}</span>
+                        </span>
+                    </span>
+                </div>
+            </div>
+            <div className="mr-0 ml-auto">
+                <div className="flex flex-row space-x-1">
+                    {actions.map((a, i) => (
+                        <IconButton key={i} icon={a.icon} color="light" onClick={a.onClick} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/gameEditor/OwnGameList_OLD.tsx
@@ -0,0 +1,68 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+import { LoadingSpinner } from "@components/util/LoadingSpinner";
+import { Modal } from "@components/util/Modal";
+import { db } from "@lib/firebase";
+import { Game } from "@lib/firebase/firestoreTypes";
+import { useOwnGameCollection } from "@lib/firebase/hooks/useOwnGameCollectionDataOnce";
+
+import { OwnGameListItem } from "./OwnGameListItem_OLD";
+
+export function OwnGameList() {
+    const { data, loading, error, couldHaveMore, more } = useOwnGameCollection();
+    const [gameToDelete, setGameToDelete] = useState<Game | null>(null);
+
+    const deleteGame = (game: Game) => {
+        deleteDoc(doc(db, "games", game.id))
+            .catch(() => console.error("Failed to delete game from database."))
+            .finally(() => setGameToDelete(null));
+    };
+
+    if (loading)
+        return (
+            <div className="flex flex-row justify-center items-center h-64">
+                <LoadingSpinner size={64} />
+            </div>
+        );
+
+    if (error || !data) return <div>An unexpected error occurred while loading games.</div>;
+
+    return (
+        <div className="text-black rounded overflow-hidden">
+            <AnimatePresence>
+                {data.map((game, i) => (
+                    <motion.div
+                        key={game.id}
+                        initial={{ opacity: 0, translateY: 10 }}
+                        animate={{ opacity: 1, translateY: 0, transition: { delay: i * 0.05 } }}
+                    >
+                        <OwnGameListItem game={game} onDeleteClick={() => setGameToDelete(game)} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+
+            {couldHaveMore && (
+                <div className="p-2 flex flex-row justify-center items-center">
+                    <div className="p-2 text-sm text-gray-400 font-bold cursor-pointer" onClick={more}>
+                        Load more ...
+                    </div>
+                </div>
+            )}
+
+            <Modal
+                open={!!gameToDelete}
+                title="Delete game?"
+                primaryBtnText="Delete"
+                secondaryBtnText="Cancel"
+                primaryBtnColor="error"
+                onPrimaryBtnClick={() => gameToDelete && deleteGame(gameToDelete)}
+                onSecondaryBtnClick={() => setGameToDelete(null)}
+                onBackgroundClick={() => setGameToDelete(null)}
+            >
+                Do you really want to delete <b>{gameToDelete?.name}</b>? This action cannot be reversed.
+            </Modal>
+        </div>
+    );
+}
